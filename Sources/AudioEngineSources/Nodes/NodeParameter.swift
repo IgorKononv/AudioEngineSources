2b4cbef8f759a03153feca43265c204b843ad746
Mithrandir <i.kononov@kissmyapps.tech>
2025-05-06 21:14:21 +0300

    3

--- Sources/AudioEngineSources/Nodes/NodeParameter.swift
+++ Sources/AudioEngineSources/Nodes/NodeParameter.swift
@@ -3,7 +3,7 @@
 import AVFoundation
 
 /// Definition or specification of a node parameter
-public struct NodeParameterDef: @unchecked Sendabl {
+public struct NodeParameterDef: @unchecked Sendable {
     /// Unique ID
     public var identifier: String
     /// Name
